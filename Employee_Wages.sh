a140d42c0c2340ce6d982730fcb870c2f7c4d17f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-25 17:50:36 +0530

    Employee Condition of days and hours are added

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,47 +1,64 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage problem"
-isPresent=1
-fullTime=1
-partTime=0
-random_Check=$((RANDOM%2))
-random_Time=$((RANDOM%2))
-empRate_PerHr=20
-empDay_PerMh=20
+IsPresent=1
+FullTime=1
+PartTime=0
+EmpRate_PerHr=20
+EmpDay_PerMh=20
+Emp_Hrs_Full=8
+Emp_Hrs_Half=4
+emp_Hrs_Full=0
+emp_Hrs_Half=0
 fullTime_Day_salary=0
 partTime_Day_salary=0
+count_full=0
+count_half=0
+while [[ ( $emp_Hrs_Full -le 100 && $count_full -le 20 ) || ( $emp_Hrs_Half -le 50 && $count_half -le 20 ) ]]
+do
+random_Check=$((RANDOM%2))
+random_Time=$((RANDOM%2))
 read -p "1).For Full Time Employee or 2).For Part Time Employee" choice
 case $choice in
 	1)
-		if [[ $isPresent == $random_Check && $fullTime == $random_Time ]]
-        	then
-			emp_Hrs=8
-        		fullTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
-		else
-        		echo "Not Present"
-        		salary=0
+		if [[ ( $emp_Hrs_Full -le 100 ) && ( $count_full -le 20 ) ]]
+		then
+			((count_full++))
+			if [[ $IsPresent -eq $random_Check && $FullTime -eq $random_Time ]]
+        		then
+				emp_Hrs_Full=$(($emp_Hrs_Full + $Emp_Hrs_Full))
+        			fullTime_Day_salary=$(( $Emp_Hrs_Full * $EmpRate_PerHr ))
+			else
+        			echo "Not Present"
+        			salary=0
+			fi
 		fi
 		;;
 	2)
-		if [[ $isPresent == $random_Check && $partTime == $random_Time ]]
+		if [[ $emp_Hrs_Half -le 50 && $count_half -le 20 ]]
 		then
-			emp_Hrs=4
-        		partTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
-		else
-        		echo "Not Present"
-        		salary=0
+			if [[ $IsPresent -eq $random_Check && $PartTime -eq $random_Time ]]
+			then
+				((count_half++))
+				emp_Hrs_Half=$(($emp_Hrs_Half + $Emp_Hrs_Half))
+	        		partTime_Day_salary=$(( $Emp_Hrs_Half * $EmpRate_PerHr ))
+			else
+	        		echo "Not Present"
+	        		salary=0
+			fi
 		fi
 		;;
 	*)
 			salary=0
 		;;
 esac
-read -p "1).For Full Time Employee Monthly or 2).For Part Time Employee Monthly" choice1
+done
+read -p "1).For Full Time Employee Monthly or 2).For Part Time Employee Monthly o Any key for Exit" choice1
 case $choice1 in
 
 	1)
 		if [ $fullTime_Day_salary != 0 ]
 		then
-			full_Time_Monthly=$(( $fullTime_Day_salary * $empDay_PerMh ))
+			full_Time_Monthly=$(( $fullTime_Day_salary * $EmpDay_PerMh ))
 		else
 			salary=0
 		fi
@@ -49,7 +66,7 @@ case $choice1 in
 	2)
 		if [ $partTime_Day_salary != 0 ]
 		then
-			part_Time_Monthly=$(( $partTime_Day_salary * $empDay_PerMh ))
+			part_Time_Monthly=$(( $partTime_Day_salary * $EmpDay_PerMh ))
 		else
                 	salary=0
                 fi
