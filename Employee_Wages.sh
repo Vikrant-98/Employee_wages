cb2010bccc41df9df7e0d105a081fb602dacdd75
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-25 16:38:14 +0530

    Solve using case statement

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -6,16 +6,29 @@ partTime=0
 random_Check=$((RANDOM%2))
 random_Time=$((RANDOM%2))
 empRate_PerHr=20
-if [[ $isPresent == $random_Check && $fullTime == $random_Time ]]
-then
-        emp_Hrs=8
+read -p "1).For Full Time Employee or 2).For Part Time Employee" choice
+case $choice in
+1)
+	if [[ $isPresent == $random_Check && $fullTime == $random_Time ]]
+        then
+	emp_Hrs=8
         fullTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
-elif [[ $isPresent == $random_Check && $partTime == $random_Time ]]
-then
-        emp_Hrs=4
-        partTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
-else
+	else
         echo "Not Present"
         salary=0
-fi
-
+	fi
+	;;
+2)
+	if [[ $isPresent == $random_Check && $partTime == $random_Time ]]
+	then
+	emp_Hrs=4
+        partTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
+	else
+        echo "Not Present"
+        salary=0
+	fi
+	;;
+*)
+	salary=0
+	;;
+esac
