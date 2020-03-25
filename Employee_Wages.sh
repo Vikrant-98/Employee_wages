752b6dfaf636dda5794e2fc5ec982015e4f91585
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-25 09:36:29 +0530

    Full Time Employee added

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -2,9 +2,14 @@
 echo "Welcome to Employee Wage problem"
 isPresent=1
 random_Check=$((RANDOM%2))
+empRate_PerHr=20
 if [ $isPresent == $random_Check ]
 then
-	echo "Present"
+        echo "Present"
+        emp_Hrs=8
+        fullTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
 else
-	echo "Not Present"
+        echo "Not Present"
+        salary=0
 fi
+
