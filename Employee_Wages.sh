54c263982f8f498595bde67f451b386aa959e82c
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-25 18:38:29 +0530

    Daily And Total Wages for Employee added along with days of work

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -24,20 +24,23 @@ emp_Hrs_Full=0
 emp_Hrs_Half=0
 fullTime_Day_salary=0
 partTime_Day_salary=0
-count_full=0
-count_half=0
-while [[ ( $emp_Hrs_Full -le 100 && $count_full -le 20 ) || ( $emp_Hrs_Half -le 50 && $count_half -le 20 ) ]]
+count_full[0]=0
+count_full[1]=0
+count_half[0]=0
+count_half[1]=0
+while [[ ( $emp_Hrs_Full -le 100 && ${count_full[0]} -le 20 ) || ( $emp_Hrs_Half -le 50 && ${count_half[0]} -le 20 ) ]]
 do
 random_Check=$((RANDOM%2))
 random_Time=$((RANDOM%2))
 read -p "1).For Full Time Employee or 2).For Part Time Employee" choice
 case $choice in
 	1)
-		if [[ ( $emp_Hrs_Full -le 100 ) && ( $count_full -le 20 ) ]]
+		if [[ ( $emp_Hrs_Full -le 100 ) && ( ${count_full[0]} -le 20 ) ]]
 		then
-			((count_full++))
+			((count_full[0]++))
 			if [[ $IsPresent -eq $random_Check && $FullTime -eq $random_Time ]]
         		then
+				((count_full[1]++))
 				Hour$()
         			fullTime_Day_salary=$(( $Emp_Hrs_Full * $EmpRate_PerHr ))
 			else
@@ -47,11 +50,12 @@ case $choice in
 		fi
 		;;
 	2)
-		if [[ $emp_Hrs_Half -le 50 && $count_half -le 20 ]]
+		if [[ $emp_Hrs_Half -le 50 && ${count_half[0]} -le 20 ]]
 		then
-			((count_half++))
+			((count_half[0]++))
 			if [[ $IsPresent -eq $random_Check && $PartTime -eq $random_Time ]]
 			then
+				((count_half[1]++))
 				Hour$()
 	        		partTime_Day_salary=$(( $Emp_Hrs_Half * $EmpRate_PerHr ))
 			else
@@ -72,6 +76,7 @@ case $choice1 in
 		if [ $fullTime_Day_salary != 0 ]
 		then
 			full_Time_Monthly=$(( $fullTime_Day_salary * $EmpDay_PerMh ))
+			echo "Monthly Wage for Full time Employee is : $full_Time_Monthly and worked for ${count_full[1]} Days"
 		else
 			salary=0
 		fi
@@ -80,6 +85,7 @@ case $choice1 in
 		if [ $partTime_Day_salary != 0 ]
 		then
 			part_Time_Monthly=$(( $partTime_Day_salary * $EmpDay_PerMh ))
+			echo "Daily Wage for Full time Employee is : $part_Time_Monthly and worked for ${count_half[1]} Days"
 		else
                 	salary=0
                 fi
@@ -97,7 +103,7 @@ case $choice1 in
 		echo "Daily Wage for Full time Employee is : $fullTime_Day_salary"
 		;;
 	2)
-		echo "Daily Wage for Full time Employee is : $part_Time_Monthly"
+		echo "Daily Wage for Full time Employee is : $partTime_Day_salary"
 		;;
 	*)
 		salary=0
