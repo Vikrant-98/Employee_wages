dea8d4e10d7fc79a2fc6cc7287d852c01b49975f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-25 09:39:45 +0530

    Part time Employee wages added

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage problem"
 isPresent=1
+fullTime=1
+partTime=0
 random_Check=$((RANDOM%2))
+random_Time=$((RANDOM%2))
 empRate_PerHr=20
-if [ $isPresent == $random_Check ]
+if [[ $isPresent == $random_Check && $fullTime == $random_Time ]]
 then
-        echo "Present"
         emp_Hrs=8
         fullTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
+elif [[ $isPresent == $random_Check && $partTime == $random_Time ]]
+then
+        emp_Hrs=4
+        partTime_Day_salary=$(( $emp_Hrs * $empRate_PerHr ))
 else
         echo "Not Present"
         salary=0
